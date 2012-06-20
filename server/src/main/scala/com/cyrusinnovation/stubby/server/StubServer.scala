1aec7a1864634433cfeca5698bfd21853faa27a2
Oliver Stewart <oliver@trailmagic.com>
2012-06-20 11:13:40 -0400

    Add basic interaction handling for path based matching

--- server/src/main/scala/com/cyrusinnovation/stubby/server/StubServer.scala
+++ server/src/main/scala/com/cyrusinnovation/stubby/server/StubServer.scala
@@ -3,14 +3,31 @@ package com.cyrusinnovation.stubby.server
 import com.twitter.finagle.Service
 import org.jboss.netty.handler.codec.http._
 import com.twitter.util.{Duration, Future}
-import java.net.{SocketAddress, InetSocketAddress}
+import java.net.{URI, SocketAddress, InetSocketAddress}
 import com.twitter.finagle.builder.{ServerBuilder, Server}
 import com.twitter.finagle.http.Http
 import java.util.concurrent.TimeUnit
 
+sealed trait RequestCondition {
+  def matches(request:HttpRequest): Boolean
+}
+case class PathCondition(path: String) extends RequestCondition {
+  def matches(request: HttpRequest) = path == URI.create(request.getUri).getPath
+}
+case class Interaction(conditions: List[RequestCondition], response: HttpResponse) {
+  def matchRequest(request: HttpRequest) = {
+    if (conditions.forall(_.matches(request))) Some(response) else None
+  }
+}
+
 class StubServer(port: Int) {
+  var interactions = List[Interaction]()
+  def addInteraction(interaction: Interaction) {
+    interactions = interaction :: interactions
+  }
+
   val service: Service[HttpRequest,HttpResponse] = new Service[HttpRequest,HttpResponse] {
-    def apply(request: HttpRequest) = Future(new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK))
+    def apply(request: HttpRequest) = Future(interactions.flatMap(_.matchRequest(request)).headOption.getOrElse(new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND)))
   }
   val address: SocketAddress = new InetSocketAddress(port)
   var server: Server = _
@@ -22,5 +39,4 @@ class StubServer(port: Int) {
   def stop() {
     server.close(Duration(10, TimeUnit.SECONDS))
   }
-
 }
--- server/src/test/scala/com/cyrusinnovation/stubby/server/StubServerTest.scala
+++ server/src/test/scala/com/cyrusinnovation/stubby/server/StubServerTest.scala
@@ -1,17 +1,62 @@
 package com.cyrusinnovation.stubby.server
 
 import org.junit.Assert._
-import org.junit.Test
-import dispatch.{url, Http}
+import org.junit.{After, Before, Test}
+import java.net.InetSocketAddress
+import com.twitter.finagle.builder.ClientBuilder
+import org.jboss.netty.handler.codec.http._
+import com.twitter.finagle.Service
+import org.jboss.netty.buffer.ChannelBuffers._
+import org.jboss.netty.util.CharsetUtil.UTF_8
+
+object SimpleResponse {
+  def apply(status: HttpResponseStatus, body: Option[String]) = {
+    val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, status)
+    body.map(body => response.setContent(copiedBuffer(body, UTF_8)))
+    response
+  }
+}
+
+object SimpleRequest {
+  def apply(method: HttpMethod, path: String, body: Option[String] = None) = {
+    val request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, method, path)
+    body.map(body => request.setContent(copiedBuffer(body, UTF_8)))
+    request
+  }
+}
 
 class StubServerTest {
-  @Test
-  def respondsToSpecifiedPort() {
-    val server = new StubServer(8082)
-    server.start()
+  var server: StubServer = _
+  var client: Service[HttpRequest, HttpResponse] = _
 
-    val http = new Http
-    val response = http x (url("http://localhost:8082/") as_str)
-    assertEquals("", response)
+  @Before
+  def setUp() {
+    server = new StubServer(8082)
+    server.start()
+    client = ClientBuilder()
+      .codec(com.twitter.finagle.http.Http())
+      .hosts(new InetSocketAddress(8082))
+      .hostConnectionLimit(1)
+      .build()
+  }
+
+  @After
+  def tearDown() {
+    server.stop()
+    client.release()
+  }
+
+  @Test
+  def respondsWith404WithNoInteractions() {
+    val response = client(SimpleRequest(HttpMethod.GET, "/")).get()
+    assertEquals(404, response.getStatus.getCode)
+  }
+
+  @Test
+  def simpleInteractionResponseBodyMatches() {
+    val body = "it works!"
+    server.addInteraction(Interaction(List(PathCondition("/")), SimpleResponse(HttpResponseStatus.OK, Some(body))))
+    val response = client(SimpleRequest(HttpMethod.GET, "/")).get()
+    assertEquals(body, response.getContent.toString(UTF_8))
   }
 }
